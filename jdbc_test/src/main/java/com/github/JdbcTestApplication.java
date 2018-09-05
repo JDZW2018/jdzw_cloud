453766fd710218d40cc2eee32cdf0ee56f01bff0
tianfusheng <tianfs@donghengwanglou.com>
2018-09-06 01:21:20 +0800

    jdbcTest init

---
+++ jdbc_test/src/main/java/com/github/JdbcTestApplication.java
@@ -0,0 +1,17 @@
+package com.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: tianfusheng
+ * @Date: 2018/9/6 01:01
+ * @Description:
+ */
+@SpringBootApplication
+public class JdbcTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcTestApplication.class, args);
+    }
+}
